2efd3c383c57def11dd0cf1c2ddd0c504b2b981f
belka <vfrosenyuk@gmail.com>
2022-02-28 23:48:48 +0400

    Доработка функций

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    throw new Error('Диапазон должен содержать только целые числа');
+  }
   if (min > max) {
-    const swap = min;
-    min = max;
-    max = swap;
+    throw new Error('Неверный диапазон');
   }
   if (min < 0) {
     throw new Error('Диапазон не может быть отрицательным');
   }
+  /*
+  Источник:
+  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  */
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomIntInclusive();
+getRandomIntInclusive(0, 5);
 
-function getLength(testComment, maxLength) {
-  const lengthComment = testComment.length;
-  if (lengthComment > maxLength) {
-    return false;
-  }
-  return true;
+function checkCommentLength(comment, MAX_LENGTH) {
+  const lengthComment = comment.length;
+  return lengthComment <= MAX_LENGTH;
 }
 
-getLength();
+checkCommentLength('Some comment', 140);
 
