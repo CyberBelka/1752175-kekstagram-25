d5ad1ab3a3fe75117d90fe8ae3c0ad888c3e7385
belka <vfrosenyuk@gmail.com>
2022-03-04 23:50:58 +0400

    Генерация данных

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Возвращает случайное целое число из переданного диапазона включительно
+
 function getRandomIntInclusive(min, max) {
   if (!Number.isInteger(min) || !Number.isInteger(max)) {
     throw new Error('Диапазон должен содержать только целые числа');
@@ -17,10 +19,80 @@ function getRandomIntInclusive(min, max) {
 
 getRandomIntInclusive(0, 5);
 
+// Проверка максимальной длины строки
+
 function checkCommentLength(comment, MAX_LENGTH) {
-  const lengthComment = comment.length;
-  return lengthComment <= MAX_LENGTH;
+  return comment.length <= MAX_LENGTH;
 }
 
 checkCommentLength('Some comment', 140);
 
+const DESCRIPTIONS =[
+  'Ещё один прекрасный день!',
+  'Лучшее место для тренировки',
+  'Бесстрашный исследователь',
+  'Вы слышали уже новость? Как вам она?',
+  'Впечатлило сильно'
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Александр',
+  'Петя',
+  'Анна',
+  'Света',
+  'Ольга',
+  'Владимир',
+  'Константин',
+  'Кристина',
+  'Лена',
+  'Аркадий'
+];
+
+// Создаёт комментарии
+
+const createComments = (number) => {
+  const comments = [];
+  const comm = [];
+  for (let j = 0; j < number; j++) {
+    comm[j] = {
+      id: (j + 1),
+    };
+  }
+  for (let i = 0; i < comm.length; i++) {
+    const comment = {
+      id: comm[i].id,
+      avatar: `img/avatar-${  getRandomIntInclusive(1, 6)  }.svg`,
+      message: MESSAGES[getRandomIntInclusive(0, MESSAGES.length - 1)],
+      name: NAMES[getRandomIntInclusive(0, NAMES.length - 1)]
+    };
+    comments.push(comment);
+  }
+  return comments;
+};
+
+// Создаёт описание фотографии
+
+const createPhoto = () => {
+  const photo = [];
+  for (let i = 0; i < 25; i++) {
+    photo[i] = {
+      id: (i + 1),
+      url: `photos/${  i + 1  }.jpg`,
+      description: DESCRIPTIONS[getRandomIntInclusive(0, DESCRIPTIONS.length - 1)],
+      likes: getRandomIntInclusive(15, 200),
+      comments: createComments(getRandomIntInclusive(0, 30))
+    };
+  }
+  return photo;
+};
+
+const similarPhotos = Array.from({length:25}, createPhoto);
