353ab91b175d9c0753d5f13678e6aa69349eed66
belka <vfrosenyuk@gmail.com>
2022-03-28 23:05:11 +0400

    Модальное окно

--- js/picture.js
+++ js/picture.js
@@ -1,19 +1,30 @@
-import {createPhotos} from './data.js';
+import {similarPhotos} from './main.js';
 
 const listPhotos = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture').content.querySelector('a');
 
-const similarPhotos = createPhotos(25);
-
-const fragment = document.createDocumentFragment();
-
-similarPhotos.forEach((photo) => {
+// TODO заменить 2-е функции (handlePictureClick, createBigPicture) на одну в параметрах
+const createPhotoElement = (photo, pictureClickHandler) => {
   const photoElement = photoTemplate.cloneNode(true);
   photoElement.querySelector('img').src = photo.url;
   photoElement.querySelector('.picture__likes').textContent = photo.likes;
   photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  fragment.appendChild(photoElement);
-});
 
-listPhotos.appendChild(fragment);
+  // Создать обработчик на click
+  photoElement.addEventListener('click', () => {
+    // Открываем модальное окно с данными
+    pictureClickHandler(photo);
+  });
+  return photoElement;
+};
 
+const renderPhotos = (pictureClickHandler) => {
+  const fragment = document.createDocumentFragment();
+  similarPhotos.forEach((photo) => {
+    listPhotos.appendChild(createPhotoElement(photo, pictureClickHandler));
+  });
+  listPhotos.appendChild(fragment);
+};
+
+
+export {renderPhotos, createPhotoElement};
