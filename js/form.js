533024f36feb5badf9bc7f89e1c66a4dd3212d1b
belka <vfrosenyuk@gmail.com>
2022-04-01 00:24:09 +0400

    Валидация

--- js/form.js
+++ js/form.js
@@ -5,6 +5,9 @@ const uploadForm = document.querySelector('.img-upload__form');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
 const uploadInput = uploadForm.querySelector('.img-upload__input');
 const uploadCancel = uploadForm.querySelector('.img-upload__cancel');
+const textHashtags = uploadForm.querySelector('.text__hashtags');
+const re = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
+const textDescription = uploadForm.querySelector('.text__description');
 
 const uploadFormEscKeydownHandler = (evt) => {
   if (isEscapeKey(evt)) {
@@ -43,17 +46,44 @@ const pristine = new Pristine(uploadForm, {
   errorClass: 'text--invalid', // Класс, обозначающий невалидное поле
   successClass: 'text--valid', // Класс, обозначающий валидное поле
   errorTextParent: 'text', // Элемент, куда будет выводиться текст с ошибкой
-  errorTextTag: 'span', // Тег, который будет обрамлять текст ошибки
+  errorTextTag: 'div', // Тег, который будет обрамлять текст ошибки
   errorTextClass: 'form__error' // Класс для элемента с текстом ошибки
 });
 
-function validateHashtags (value) {
-  return value.length >= 2 && value.length <= 20;
-}
+pristine.addValidator(textHashtags, () => {
+  const arrayHashtags = textHashtags.value.split(' ');
+  if (arrayHashtags.length > 5) {
+    return false;
+  }
+  return true;
+}, 'Не более 5 хэш-тегов');
 
-pristine.addValidator(uploadForm.querySelector('.text__hashtags'), validateHashtags, 'От 2 до 20 символов');
+pristine.addValidator(textHashtags, () => {
+  const arrayHashtags = textHashtags.value.split(' ');
+  for (let i = 0; i < arrayHashtags.length; i++) {
+    if (!re.test(arrayHashtags[i]) && arrayHashtags[i] !== '') {
+      return false;
+    }
+  }
+  return true;
+}, 'Некорректный хэш-тег');
+
+pristine.addValidator(textHashtags, () => {
+  const arrayHashtags = textHashtags.value.split(' ');
+  for (let i = 0; i < arrayHashtags.length; i++) {
+    for (let j = i + 1; j < arrayHashtags.length; j++) {
+      if (arrayHashtags[i].toLowerCase() === arrayHashtags[j].toLowerCase() && arrayHashtags[i] !== '') {
+        return false;
+      }
+    }
+  }
+  return true;
+}, 'Хэш-теги не должны повторяться');
+
+pristine.addValidator(textDescription, () => textDescription.value.length <= 140, 'Не более 140 символов');
 
 uploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
 });
